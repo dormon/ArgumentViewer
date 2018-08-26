eef599992057175f8f5992496e9b8e198f9c6039
dormon <imilet@fit.vutbr.cz>
2018-08-26 05:31:11 +0200

    Refactoring ContextFormat

--- src/ArgumentViewer/private/ContextFormat.cpp
+++ src/ArgumentViewer/private/ContextFormat.cpp
@@ -39,25 +39,6 @@ void throwIfArgumentIsNotContextBegin(vector<string> const &args,size_t &index,s
   throw MatchError(ss.str());
 }
 
-void throwIfUnusedFormatsIsEmpty(vector<string> const &args,size_t &index,set<string>const&unusedFormats,string const&argumentName){
-  if (!unusedFormats.empty()) return;
-  stringstream ss;
-  ss << "Argument error:" << endl;
-  ss << "following arguments do not belong in context: " << argumentName
-     << ": " << endl;
-  for (size_t i = index; i < args.size(); ++i) ss << args.at(i) << " ";
-  throw MatchError(ss.str());
-}
-
-void throwIfFromatForRemovalIsEmpty(string const&formatForRemoval,vector<string>const&args,size_t&index,string const&argumentName){
-  if (formatForRemoval != "") return;
-  stringstream ss;
-  ss << "Argument error:" << endl;
-  ss << "argument: " << args.at(index)
-     << " does not belong in context: " << argumentName;
-  throw MatchError(ss.str());
-}
-
 void throwIfArgumentsUbruptlyEnds(vector<string>const&args,size_t&index,string const&argumentName){
   if (index < args.size()) return;
   stringstream ss;
@@ -65,16 +46,6 @@ void throwIfArgumentsUbruptlyEnds(vector<string>const&args,size_t&index,string c
   ss << "expected " << contextEnd
      << " at the end of context: " << argumentName;
   ss << " not end of arguments";
-  throw MatchError(ss.str());
-}
-
-void throwIfArgumentsDoNotContainContextEnd(vector<string>const&args,size_t&index,string const&argumentName){
-  if (args.at(index) == contextEnd)return;
-  stringstream ss;
-  ss << "Argument error:" << endl;
-  ss << "expected " << contextEnd
-     << " at the end of context: " << argumentName;
-  ss << " not argument: " << args.at(index);
   throw MatchError(ss.str());
 }
 
@@ -87,23 +58,12 @@ Format::MatchStatus ContextFormat::match(vector<string> const &args,
   throwIfIndexIsOutOfRange(args,index,argumentName);
   throwIfArgumentIsNotContextBegin(args,index,argumentName);
   ++index;
-  set<string> unusedFormats;
-  for (auto const &x : formats) unusedFormats.insert(x.first);
+  auto unusedFormats = getUnusedFormats();
   while (index < args.size() &&
          args.at(index) != contextEnd) {
-    throwIfUnusedFormatsIsEmpty(args,index,unusedFormats,argumentName);
-    string formatForRemoval = "";
-    for (auto const &f : unusedFormats) {
-      auto status = formats.at(f)->match(args, index);
-      if (status == MATCH_FAILURE) continue;
-      formatForRemoval = f;
-      break;
-    }
-    throwIfFromatForRemovalIsEmpty(formatForRemoval,args,index,argumentName);
-    unusedFormats.erase(formatForRemoval);
+    checkAndMatchOneUnusedFormat(unusedFormats,args,index);
   }
   throwIfArgumentsUbruptlyEnds(args,index,argumentName);
-  throwIfArgumentsDoNotContainContextEnd(args,index,argumentName);
   ++index;
   return MATCH_SUCCESS;
 }
